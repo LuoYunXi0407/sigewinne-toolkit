8d751e24599757f7c92e2efc054354ebc7dd8a38
Administrator <Administrator>
2025-12-10 19:39:51 +0800

    add settings

--- App6/Settings.h
+++ App6/Settings.h
@@ -1,5 +1,36 @@
 #pragma once
 namespace Service::Settings
 {
-	
+	namespace Homepage
+	{
+		struct LaunchGame
+		{
+			char gamePath[260];
+		};
+		struct Island
+		{
+            BOOL  EnableSetFieldOfView;
+            FLOAT FieldOfView;
+            BOOL  FixLowFovScene;
+            BOOL  DisableFog;
+            BOOL  EnableSetTargetFrameRate;
+            DWORD TargetFrameRate;
+            BOOL  RemoveOpenTeamProgress;
+            BOOL  HideQuestBanner;
+            BOOL  DisableEventCameraMove;
+            BOOL  DisableShowDamageText;
+            BOOL  UsingTouchScreen;
+            BOOL  RedirectCombineEntry;
+            BOOL  ResinListItemId000106Allowed;
+            BOOL  ResinListItemId000201Allowed;
+            BOOL  ResinListItemId107009Allowed;
+            BOOL  ResinListItemId107012Allowed;
+            BOOL  ResinListItemId220007Allowed;
+		};
+	}
+
+	namespace About
+	{
+		BOOL ShowOnStartup = TRUE;
+	}
 }
