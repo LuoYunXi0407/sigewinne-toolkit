a3d07a5b9ba8722ab096ea01cf909cd000e32eab
Administrator <Administrator>
2025-12-19 19:54:22 +0800

    finish button config

--- App6/LaunchGamePage.xaml.cpp
+++ App6/LaunchGamePage.xaml.cpp
@@ -16,74 +16,91 @@ namespace winrt::App6::implementation
 {
 	bool LaunchGamePage::LaunchGameWindowsHDR()
 	{
-		return p
+		return plaunchgame->iswindowshdrenabled();
 	}
 
 	void LaunchGamePage::LaunchGameWindowsHDR(bool value)
 	{
+		plaunchgame->set_iswindowshdrenabled(value);
 	}
 
 	bool LaunchGamePage::LaunchGameArguments()
 	{
+		return plaunchgame->arecommandlineargumentsenabled();
 	}
 
 	void LaunchGamePage::LaunchGameArguments(bool value)
 	{
+		plaunchgame->set_arecommandlineargumentsenabled(value);
 	}
 
 	bool LaunchGamePage::LaunchGameAppearanceExclusive()
 	{
+		return plaunchgame->isexclusive();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceExclusive(bool value)
 	{
+		plaunchgame->set_isexclusive(value);
 	}
 
 	bool LaunchGamePage::LaunchGameAppearanceFullscreen()
 	{
+		return plaunchgame->isfullscreen();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceFullscreen(bool value)
 	{
+		plaunchgame->set_isfullscreen(value);
 	}
 
 	bool LaunchGamePage::LaunchGameAppearanceBorderless()
 	{
+		return plaunchgame->isborderless();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceBorderless(bool value)
 	{
+		plaunchgame->set_isborderless(value);
 	}
 
 	bool LaunchGamePage::LaunchGameAppearanceScreenWidth()
 	{
+		return plaunchgame->isscreenwidthenabled();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceScreenWidth(bool value)
 	{
+		plaunchgame->set_isscreenwidthenabled(value);
 	}
 
 	bool LaunchGamePage::LaunchGameAppearanceScreenHeight()
 	{
+		return plaunchgame->isscreenheightenabled();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceScreenHeight(bool value)
 	{
+		plaunchgame->set_isscreenheightenabled(value);
 	}
 
 	uint32_t LaunchGamePage::LaunchGameAppearanceScreenWidthValue()
 	{
+		return plaunchgame->screenwidth();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceScreenWidthValue(uint32_t value)
 	{
+		plaunchgame->set_screenwidth(value);
 	}
 
 	uint32_t LaunchGamePage::LaunchGameAppearanceScreenHeightValue()
 	{
+		return plaunchgame->screenheight();
 	}
 
 	void LaunchGamePage::LaunchGameAppearanceScreenHeightValue(uint32_t value)
 	{
+		plaunchgame->set_screenheight(value);
 	}
 }
