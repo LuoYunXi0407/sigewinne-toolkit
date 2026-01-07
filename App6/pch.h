b910a821ab34d734e8afc0eb11f0b4144e83ed4e
sigewinnefish <sigewinne_fish@outlook.com>
2026-01-07 23:28:48 +0800

    add more headers to pch

--- App6/pch.h
+++ App6/pch.h
@@ -19,9 +19,15 @@
 #include <winrt/Microsoft.UI.Xaml.Interop.h>
 #include <winrt/Microsoft.UI.Xaml.Markup.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.Media.Animation.h>
 #include <winrt/Microsoft.UI.Xaml.Navigation.h>
 #include <winrt/Microsoft.UI.Xaml.Shapes.h>
 #include <winrt/Microsoft.UI.Dispatching.h>
+#include <winrt/Microsoft.UI.Windowing.h>
+#include <winrt/Microsoft.Windows.Globalization.h>
+#include <winrt/Microsoft.Windows.Storage.Pickers.h>
+#include <winrt/Microsoft.Windows.ApplicationModel.Resources.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
 #include <wil/cppwinrt_helpers.h>
 
 // WinUIEssential.WinUI3
